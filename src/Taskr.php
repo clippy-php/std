07a4b13deec39d6d8345ad2508e2a03dd29cb21e
Tim Otten <totten@civicrm.org>
2023-02-02 00:49:26 -0800

    Taskr - Add support for subcommands

--- src/Taskr.php
+++ src/Taskr.php
@@ -3,9 +3,8 @@ namespace Clippy;
 
 use Clippy\Exception\UserQuitException;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
-use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\StyleInterface;
 
 /**
  * The `Taskr` ("tasker") is a small utility for writing adhoc scripts that run
@@ -25,75 +24,61 @@ use Symfony\Component\Console\Style\StyleInterface;
  */
 class Taskr {
 
-  /**
-   * @param \Symfony\Component\Console\Style\StyleInterface $io
-   * @param \Symfony\Component\Console\Input\InputInterface $input
-   * @param \Symfony\Component\Console\Output\OutputInterface $output
-   * @param \Clippy\Cmdr $cmdr
-   */
-  public function __construct(StyleInterface $io, InputInterface $input, OutputInterface $output, Cmdr $cmdr) {
-    $this->io = $io;
-    $this->input = $input;
-    $this->output = $output;
-    $this->cmdr = $cmdr;
+  public function __construct(Container $c) {
+    $this->c = $c;
   }
 
   public static function register(Container $c): Container {
-    $c->set('taskr', function (StyleInterface $io, InputInterface $input, OutputInterface $output, Cmdr $cmdr) {
-      return new Taskr($io, $input, $output, $cmdr);
+    $c->set('taskr', function () use ($c) {
+      return new Taskr($c);
     });
     return $c;
   }
 
   /**
-   * @var \Symfony\Component\Console\Style\StyleInterface
+   * @var \Psr\Container\ContainerInterface
    */
-  protected $io;
-
-  /**
-   * @var \Symfony\Component\Console\Input\InputInterface
-   */
-  protected $input;
-
-  /**
-   * @var \Symfony\Component\Console\Output\OutputInterface
-   */
-  protected $output;
-
-  /**
-   * @var Cmdr
-   */
-  protected $cmdr;
-
-  protected $isChecked = FALSE;
+  protected $c;
 
   public function assertConfigured(): void {
-    if ($this->isChecked) {
-      return;
-    }
+    $c = $this->c;
 
     $missingOptions = [];
-    if (!$this->input->hasOption('step')) {
+    if (!$c['input']->hasOption('step')) {
       $missingOptions[] = '--step';
     }
-    if (!$this->input->hasOption('dry-run')) {
+    if (!$c['input']->hasOption('dry-run')) {
       $missingOptions[] = '--dry-run';
     }
     if (!empty($missingOptions)) {
       trigger_error("Command uses 'Taskr', but the expected options (" . implode(' ', $missingOptions) . ") have not been pre-defined.", E_USER_WARNING);
     }
-
-    $this->isChecked = TRUE;
   }
 
-  public function passthru(string $cmd, array $params = []) {
+  /**
+   * Call an external command and pass-through all inputs and outputs.
+   *
+   * @param string $cmd
+   * @param array $params
+   * @throws \Clippy\Exception\CmdrProcessException
+   */
+  public function passthru(string $cmd, array $params = []): void {
     $this->assertConfigured();
 
-    $io = $this->io;
-    $extraVerbosity = 0;
-    $cmdDesc = '<comment>$</comment> ' . $this->cmdr->escape($cmd, $params) . ' <comment>[[in ' . getcwd() . ']]</comment>';
+    // We resolve these variables as-needed because that works better with recursive command invocations.
 
-    if ($this->input->hasOption('step') && $this->input->getOption('step')) {
+    /**
+     * @var \Symfony\Component\Console\Style\StyleInterface $io
+     * @var \Symfony\Component\Console\Input\InputInterface $input
+     * @var Cmdr $cmdr
+     */
+    $io = $this->c['io'];
+    $input = $this->c['input'];
+    $cmdr = $this->c['cmdr'];
+    $extraVerbosity = 0;
+    $cmdDesc = '<comment>$</comment> ' . $cmdr->escape($cmd, $params) . ' <comment>[[in ' . getcwd() . ']]</comment>';
+
+    if ($input->hasOption('step') && $input->getOption('step')) {
       $extraVerbosity = OutputInterface::VERBOSITY_VERBOSE - $io->getVerbosity();
       $io->writeln('<comment>COMMAND</comment>' . $cmdDesc);
       $confirmation = ($io->ask('<info>Execute this command?</info> (<comment>[Y]</comment>es, <comment>[s]</comment>kip, <comment>[q]</comment>uit)', NULL, function ($value) {
@@ -117,16 +102,67 @@ class Taskr {
       }
     }
 
-    if ($this->input->hasOption('dry-run') && $this->input->getOption('dry-run')) {
+    if ($input->hasOption('dry-run') && $input->getOption('dry-run')) {
       $io->writeln('<comment>DRY-RUN</comment>' . $cmdDesc);
       return;
     }
 
     try {
       $io->setVerbosity($io->getVerbosity() + $extraVerbosity);
-      $this->cmdr->passthru($cmd, $params);
+      $cmdr->passthru($cmd, $params);
     } finally {
       $io->setVerbosity($io->getVerbosity() - $extraVerbosity);
+    }
+  }
+
+  /**
+   * Call a different subcommand (within the same application / same process).
+   *
+   * Note: The $input will be temporarily replaced with a new $input, based on the requested command options.
+   *
+   * @param string $cmd
+   *   Ex: 'greet --msg={{MSG|s}}'
+   * @param array $params
+   *   Ex: ['MSG' => 'hello world']
+   * @throws \Exception
+   */
+  public function subcommand(string $cmd, array $params = []): void {
+
+    // We resolve these variables as-needed because that works better with recursive command invocations.
+
+    /**
+     * @var \Clippy\Application $app
+     * @var \Symfony\Component\Console\Input\InputInterface $origInput
+     * @var \Symfony\Component\Console\Output\OutputInterface $output
+     * @var \Clippy\Cmdr $cmdr
+     */
+    $app = $this->c['app'];
+    $origInput = $this->c['input'];
+    $output = $this->c['output'];
+    $cmdr = $this->c['cmdr'];
+
+    $cmdParts = explode(' ', $cmd, 2);
+    foreach (['dry-run', 'step'] as $propagateOption) {
+      if ($origInput->hasOption($propagateOption) && $origInput->getOption($propagateOption)) {
+        array_splice($cmdParts, 1, 0, ["--{$propagateOption}"]);
+      }
+    }
+    $fullCmd = $cmdr->escape(implode(' ', $cmdParts), $params);
+    if ($output->isVerbose()) {
+      $output->write('<comment>SUBCOMMAND$</comment> ');
+      $output->writeln($fullCmd, OutputInterface::OUTPUT_RAW);
+    }
+
+    $stringInput = new StringInput($fullCmd);
+    $command = $app->find($stringInput->getFirstArgument());
+    try {
+      $this->c['input'] = $stringInput;
+      $result = $command->run($stringInput, $output);
+      if (!empty($result)) {
+        throw new \RuntimeException("Subcommand failed ($fullCmd) => ($result)");
+      }
+    } finally {
+      $this->c['input'] = $origInput;
     }
   }
 
